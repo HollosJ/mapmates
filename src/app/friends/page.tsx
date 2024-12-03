52d71eb83a2deab6bac65c8df7c3eaa16538ff77
HollosJ <hollosj99@gmail.com>
2024-12-03 23:47:30 +0000

    Add placeholder friends page

---
+++ src/app/friends/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function FriendsPage() {
+  return (
+    <div className="container py-8 md:py-16">
+      <h1 className="text-3xl font-black text-white md:text-5xl">
+        My Map Mates
+      </h1>
+
+      <p className="mt-8 text-lg text-white">Coming soon!</p>
+    </div>
+  );
+}
