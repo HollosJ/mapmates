aab0ff344a7e2831c7c879ef23e22dc5f78fe628
HollosJ <hollosj99@gmail.com>
2024-12-04 00:24:38 +0000

    Add promise type

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -4,6 +4,8 @@ import { DBUser } from '@/types';
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
 
+type Params = Promise<{ id: string }>;
+
 async function getUserById(id: string): Promise<DBUser | null> {
   return prisma.user.findUnique({
     where: { id },
@@ -12,7 +14,7 @@ async function getUserById(id: string): Promise<DBUser | null> {
 
 // Dynamically set metadata for the page based on the users name
 // https://nextjs.org/docs/app/api-reference/functions/generate-metadata
-export async function generateMetadata({ params }: { params: { id: string } }) {
+export async function generateMetadata({ params }: { params: Params }) {
   const { id } = await params;
   const user = await getUserById(id);
 
@@ -22,7 +24,7 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
   };
 }
 
-export default async function Page({ params }: { params: { id: string } }) {
+export default async function Page({ params }: { params: Params }) {
   // Get the user based on the ID in the URL params
   const { id } = await params;
   const user = await getUserById(id);
@@ -43,8 +45,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     });
   }
 
-  // Handle case where user is not found
-  if (!user) notFound(); // Triggers Next.js 404 page
+  if (!user) notFound();
 
   return (
     <div className="container px-8 py-16">
