1b51057e8d3e02ddc90c828757f087972df6d34e
HollosJ <hollosj99@gmail.com>
2024-12-13 13:34:40 +0000

    Add edit profile page

---
+++ src/app/profile/edit/page.tsx
@@ -0,0 +1,13 @@
+import EditProfileForm from '@/components/EditProfileForm';
+
+export default async function EditProfilePage() {
+  return (
+    <div className="container py-8 md:py-16">
+      <div className="bg-white rounded border shadow md:p-8 p-4 flex flex-col items-center md:max-w-screen-sm mx-auto">
+        <h1 className="mt-4 text-2xl font-bold">Edit Profile</h1>
+
+        <EditProfileForm />
+      </div>
+    </div>
+  );
+}
